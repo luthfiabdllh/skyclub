7c7e2052a1d1badfbc1c5900c3bbd86d00dcdca7
prandika119 <an.prasetya9@gmail.com>
2024-10-12 09:00:30 +0700

    add backend - login - register -  forgot psw

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function authentication(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+        // pesan error ketika login gagal, akan balik ke halaman login;
+        return back()->with('error', 'Login Gagal');
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
---
+++ app/Http/Controllers/auth/SetPasswordController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class SetPasswordController extends Controller
+{
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('auth.set_password', compact('user'));
+    }
+    public function update(Request $request, $id)
+    {
+        $password = $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+        $password['password'] = Hash::make($password['password']);
+        User::where('id', $id)->update($password);
+        return redirect()->route('login.index')->withSuccess('Update Password berhasil, silahkan login!');
+    }
+}
