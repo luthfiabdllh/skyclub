baeeabe012361c0278e4a1460fd08b59a38cb19d
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-10-25 11:12:33 +0700

    commit

--- resources/views/buktiPembayaran.blade.php
+++ resources/views/buktiPembayaran.blade.php
@@ -7,51 +7,50 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
-<body class="h-full">
+<body class="min-h-full">
     <x-navbar></x-navbar>
-    <div class="my-12 relative">
+    <div class="my-10">
         <div class="border bg-white shadow p-5 rounded-xl px-[70px] py-[40px] mx-auto w-[765px] space-y-10">
-            <div class="flex flex-col items-center text-center">
-                <p>Selesaikan pembayaran sebelum</p>
-                <div class="text-red-600 bg-teal-500 w-fit px-3 py-2 rounded-xl space-x-1 flex items-center">
+            <div class="flex flex-col items-center text-center font-bold">
+                <p class=" text-2xl ">Selesaikan pembayaran sebelum</p>
+                <div class="text-red-600 bg-teal-500 w-fit px-3 py-2 rounded-xl space-x-1 flex items-center text-2xl my-2">
                     <svg class="w-6 h-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                       </svg>
-
                     <p>5:00</p>
                 </div>
-                <p>Kirim Bukti Pembayaran Sebelum</p>
-                <p>Rabu, 18 September 2024 18:44</p>
+                <p class=" text-lg">Kirim Bukti Pembayaran Sebelum</p>
+                <p class=" text-lg">Rabu, 18 September 2024 18:44</p>
             </div>
-            <div class="p-3 border border-gray-500 rounded-lg">
+            <div class="p-3 border border-gray-500 rounded-lg text-lg">
                 <div class="flex justify-between items-center">
                     <p>Transfer Bank</p>
                     <img src="{{Storage::url('images/Transfer_bank.svg')}}" alt="">
                 </div>
                 <hr class="h-px my-4 bg-gray-400 border-0 dark:bg-gray-700">
                 <div class="flex justify-between items-center">
-                    <div>
+                    <div class=" space-y-1">
                         <p>Nomor Rekening</p>
-                        <p>45663245</p>
+                        <p class=" font-bold">45663245</p>
                     </div>
-                    <div class="flex">
-                        <p>Salin</p>
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <a href="/" class="flex font-semibold">
+                        <span>Salin</span>
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M9 8v3a1 1 0 0 1-1 1H5m11 4h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1v1m4 3v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-7.13a1 1 0 0 1 .24-.65L7.7 8.35A1 1 0 0 1 8.46 8H13a1 1 0 0 1 1 1Z"/>
                           </svg>
-                    </div>
+                    </a>
                 </div>
                 <hr class="h-px my-4 bg-gray-400 border-0 dark:bg-gray-700">
                 <div class="flex justify-between items-center">
-                    <div>
+                    <div class=" space-y-1">
                         <p>Total Tagihan</p>
-                        <p>Rp. 705.000</p>
+                        <p class=" font-bold">Rp. 705.000</p>
                     </div>
-                    <p>Lihat Detail</p>
+                    <a href="/" class="font-semibold">Lihat Detail</a>
                 </div>
             </div>
-            <div class="p-3 border border-gray-400 rounded-lg space-y-1">
-                <p>Upload Bukti Pembayaran</p>
+            <div class="p-3 border border-gray-400 rounded-lg space-y-2">
+                <p class=" text-lg">Upload Bukti Pembayaran</p>
                 <div class="flex items-center justify-center w-full">
                     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500">
                         <div class="flex flex-col items-center justify-center pt-5 pb-6">
@@ -70,6 +69,7 @@
                 Kirim
             </button>
         </div>
+    </div>
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
