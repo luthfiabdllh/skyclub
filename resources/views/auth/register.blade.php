1c4990b8a41a8fdf2dc94b239781e83880ab85ab
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-10-25 15:30:06 +0700

    commit setelah pad

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,7 +27,7 @@
                             <input type="text" name="name" placeholder="name"
                                 class="w-full block px-2.5 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600
                                 @error('name')
-                                    bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500
+                                    bg-red-50 border  focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500
                                 @enderror"
                                 value="{{ old('name') }}" required />
                             @error('name')
