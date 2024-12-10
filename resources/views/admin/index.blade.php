915712fe48cf5fc56cec3424e32bc4ecab75dae0
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-12-10 13:36:50 +0700

    tampilan

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -134,15 +134,10 @@
                                                         </svg>
                                                         <span class="sr-only">Close modal</span>
                                                     </button>
-                                                    <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
-                                                        aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                                                        xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd"
-                                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                            clip-rule="evenodd"></path>
+                                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                                     </svg>
-                                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want
-                                                        to delete this item?</p>
+                                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Apakah anda yakin akan menerima booking?</p>
                                                     <div class="flex justify-center items-center space-x-4">
                                                         <form action="{{ route('admin.acceptBooking', $booking->id) }}"
                                                             method="POST" class="inline">
@@ -236,15 +231,10 @@
                                                         </svg>
                                                         <span class="sr-only">Close modal</span>
                                                     </button>
-                                                    <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
-                                                        aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                                                        xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd"
-                                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                            clip-rule="evenodd"></path>
+                                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                                     </svg>
-                                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want
-                                                        to delete this item?</p>
+                                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Apakah anda yakin menerima pembatalan jadwal?</p>
                                                     <div class="flex justify-center items-center space-x-4">
                                                         <form
                                                             action="{{ route('admin.acceptReschedule', ['request' => $reschedule->id, 'listBooking' => $reschedule->id_list_booking]) }}"
@@ -340,15 +330,10 @@
                                                         </svg>
                                                         <span class="sr-only">Close modal</span>
                                                     </button>
-                                                    <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
-                                                        aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                                                        xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd"
-                                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                            clip-rule="evenodd"></path>
+                                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                                     </svg>
-                                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want
-                                                        to delete this item?</p>
+                                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Apakah anda yakin akan membatalkan pesanan?</p>
                                                     <div class="flex justify-center items-center space-x-4">
                                                         <form
                                                             action="{{ route('admin.acceptCancelBooking', $listBooking->id) }}"
