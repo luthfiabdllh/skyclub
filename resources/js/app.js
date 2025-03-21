259aa97dd6adb79abf46e0eccb6cc06011f31af7
prandika119 <an.prasetya9@gmail.com>
2025-03-21 11:05:38 +0700

    fix: import app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,15 @@
-import './bootstrap';
-import 'flowbite';
-import Alpine from 'alpinejs';
+import "./bootstrap";
+import "flowbite";
+import Alpine from "alpinejs";
+import "./chart";
+import "./descriptionField";
+import "./dropzoneFieldPhoto";
+import "./dropzonePayment";
+import "./editor";
+import "./editorUpdate";
+import "./tableArticle";
+import "./tableBooking";
+import "./tableVoucher";
 
 window.Alpine = Alpine;
 Alpine.start();
